61ec7c723fde386974dc458b0061b0c34163277f
Thayná Bittencourt Baima <saidai_thayna@hotmail.com>
2023-12-02 20:42:18 -0300

    criou a tabela

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
+const User = require('./models/Users');
 
-const db = require('./models/db')
 
 app.get("/", async (req, res) => {
     res.send("Página inicial -  Bittencourt Baimyná");
 });
 
+app.post("/cadastrar", async (req, res) => {
+   res.send("Página Cadastrar");
+});
+
 app.listen(4096, () => {
-    console.log("Servidor acessado na porta 4096: http://localhost:4096");
+    console.log("Servidor acessado na porta 4096: ");
 });
---
+++ models/Users.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize');
+const db = require('./db');
+const sequelize = require('./db');
+
+const User = db.define('users', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    name:{
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    email:{
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+
+});
+
+//Criar a tabela
+User.sync();
+//Verificar se há alguma diferença na tabela, realiza a alteração
+//User.sync({ alter: true })
+
+module.exports = User
