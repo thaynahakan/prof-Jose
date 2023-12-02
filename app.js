fb403106c2ef49bd986e46e22403a32c8a5568c0
Thayná Bittencourt Baima <saidai_thayna@hotmail.com>
2023-12-02 15:17:00 -0300

    atualização

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
- const app = express();
+const app = express();
 
  app.get("/", async (req, res) => {
     res.send("Página inicial");
- });
+ }); 
 
  app.listen(8480, () => {
     console.log("Servidor iniciado na porta 8480: http://localhost:8480");
