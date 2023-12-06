d31eb0481088750134c17d4cdff2e54777e636d6
Thayna Bittencourt Baima <131307382+thaynahakan@users.noreply.github.com>
2023-12-05 21:02:01 -0300

    Update Users.js

--- models/Users.js
+++ models/Users.js
@@ -1,7 +1,11 @@
+// Importa o Sequelize para interagir com o banco de dados
 const Sequelize = require('sequelize');
+
+// Importa uma instância previamente configurada do Sequelize (de um arquivo db.js, por exemplo)
 const db = require('./db');
 const sequelize = require('./db');
 
+// Define o modelo 'User' que representa a tabela 'users' no banco de dados
 const User = db.define('users', {
     id: {
         type: Sequelize.INTEGER,
@@ -17,12 +21,15 @@ const User = db.define('users', {
         type: Sequelize.STRING,
         allowNull: false,
     },
-
 });
 
-//Criar a tabela
-//User.sync();
-//Verificar se há alguma diferença na tabela, realiza a alteração
-//User.sync({ alter: true })
+// Os métodos abaixo são usados para sincronizar o modelo com o banco de dados
+// User.sync() cria a tabela se ela ainda não existe
+// User.sync({ alter: true }) verifica e realiza alterações na tabela com base no modelo
+// No entanto, eles estão comentados, portanto, não estão sendo executados atualmente
 
-module.exports = User
+// User.sync();
+// User.sync({ alter: true })
+
+// Exporta o modelo 'User' para ser usado em outros arquivos do projeto
+module.exports = User;
